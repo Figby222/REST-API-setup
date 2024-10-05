88174f92be08551b220ce7320b6523fb3c8cc1f9
Figby222 <figby222@outlook.com>
2024-10-05 08:03:27 -0700

    Add index.mjs

---
+++ routers/index.mjs
@@ -0,0 +1,11 @@
+import indexRouter from "./indexRouter.mjs";
+import sessionsRouter from "./sessionRouter.mjs";
+import usersRouter from "./usersRouter.mjs";
+import messagesRouter from "./messagesRouter.mjs";
+
+export default {
+    indexRouter,
+    sessionsRouter,
+    usersRouter,
+    messagesRouter,
+};
