6bf56a44b835988f2d66470fdf847ccd0bd4a219
Figby222 <figby222@outlook.com>
2024-10-05 06:32:24 -0700

    Add /messages/:messageId DELETE route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -20,7 +20,5 @@ indexRouter.get("/messages", indexController.messagesListGet);
 indexRouter.get("/messages/:messageId", indexController.messageDetailsGet);
 
 indexRouter.post("/messages", indexController.createMessagePost);
-
-
-
+indexRouter.delete("/messages/:messageId", indexController.messageDelete);
 export default indexRouter;
