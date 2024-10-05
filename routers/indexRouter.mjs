17981bc4762c1ae4e7cd039482b445005e805598
Figby222 <figby222@outlook.com>
2024-10-05 06:38:58 -0700

    Add /session GET route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -20,5 +20,9 @@ indexRouter.get("/messages", indexController.messagesListGet);
 indexRouter.get("/messages/:messageId", indexController.messageDetailsGet);
 
 indexRouter.post("/messages", indexController.createMessagePost);
+
 indexRouter.delete("/messages/:messageId", indexController.messageDelete);
+
+indexRouter.get("/session", indexController.sessionInfoGet);
+
 export default indexRouter;
