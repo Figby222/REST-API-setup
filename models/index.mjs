feaac220f37bb55def2ce1231293f0497086f202
Figby222 <figby222@outlook.com>
2024-10-05 07:21:20 -0700

    Add index.mjs

---
+++ models/index.mjs
@@ -0,0 +1,46 @@
+const users = {
+    1: {
+        id: "1",
+        username: "CoolGuy123",
+    },
+    2: {
+        id: "2",
+        username: "AwesomeSandwich",
+    },
+    3: {
+        id: "3",
+        username: "SandwichGuy",
+    },
+    4: {
+        id: "4",
+        username: "SandwichCookies",
+    },
+};
+
+let messages = {
+    1: {
+        id: "1",
+        text: "Hello Sandwich",
+        userId: "1",
+    },
+    2: {
+        id: "2",
+        text: "Hello CoolGuy",
+        userId: "2",
+    },
+    3: {
+        id: "3",
+        text: "Hello Cookies",
+        userId: "3"
+    },
+    4: {
+        id: "4",
+        text: "Hello Sandwich",
+        userId: "4",
+    },
+};
+
+export default {
+    users,
+    messages
+};
