bad4acc1a9ab0b43a010843be8326bd7a6719127
Figby222 <figby222@outlook.com>
2024-10-05 07:22:01 -0700

    Remove sample data

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,47 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 
-const users = {
-    1: {
-        id: "1",
-        username: "CoolGuy123",
-    },
-    2: {
-        id: "2",
-        username: "AwesomeSandwich",
-    },
-    3: {
-        id: "3",
-        username: "SandwichGuy",
-    },
-    4: {
-        id: "4",
-        username: "SandwichCookies",
-    },
-};
-
-let messages = {
-    1: {
-        id: "1",
-        text: "Hello Sandwich",
-        userId: "1",
-    },
-    2: {
-        id: "2",
-        text: "Hello CoolGuy",
-        userId: "2",
-    },
-    3: {
-        id: "3",
-        text: "Hello Cookies",
-        userId: "3"
-    },
-    4: {
-        id: "4",
-        text: "Hello Sandwich",
-        userId: "4",
-    },
-};
-
 function myMiddleware(req, res, next) {
     req.me = users[4];
     next();
