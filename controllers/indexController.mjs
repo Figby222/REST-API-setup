248d0bc2c8e64391a7eda4514ce818beb914eab8
Figby222 <figby222@outlook.com>
2024-10-05 04:59:46 -0700

    Add users & messages

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,3 +1,27 @@
+const users = {
+    1: {
+        id: "1",
+        username: "CoolGuy123",
+    },
+    2: {
+        id: "2",
+        username: "AwesomeSandwich",
+    },
+};
+
+const messages = {
+    1: {
+        id: "1",
+        text: "Hello Sandwich",
+        userId: "1",
+    },
+    2: {
+        id: "2",
+        text: "Hello CoolGuy",
+        userId: "2",
+    },
+};
+
 function indexRouteGet(req, res) {
     res.render("index", { title: "Node Template" });
 }
