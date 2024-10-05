83c56c8d6665bbc164927d302d4b74715d0ea23a
Figby222 <figby222@outlook.com>
2024-10-05 04:45:13 -0700

    Log userId in indexRoutePut() & indexRouteDelete()
    Log userId in indexRoutePut() & indexRouteDelete()
    controllers

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -7,11 +7,11 @@ function indexRoutePost(req, res) {
 }
 
 function indexRoutePut(req, res) {
-    return res.send("Received PUT HTTP method");
+    return res.send(`Received PUT HTTP method for user with id: ${req.params.userId}`);
 }
 
 function indexRouteDelete(req, res) {
-    return res.send("Received DELETE HTTP method");
+    return res.send(`Received DELETE HTTP method for user with id: ${req.params.userId}`);
 }
 
 export { indexRouteGet, indexRoutePost, indexRoutePut, indexRouteDelete };
