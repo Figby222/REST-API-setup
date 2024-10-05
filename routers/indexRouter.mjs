27ffb69595842505dd7cdde101d0fe2555d3fae2
Figby222 <figby222@outlook.com>
2024-10-05 08:21:47 -0700

    Move /messages routes to messagesRouter

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -1,14 +1,4 @@
 import { Router } from "express";
 import * as indexController from "../controllers/indexController.mjs";
-
 const indexRouter = Router();
-
-indexRouter.get("/messages", indexController.messagesListGet);
-
-indexRouter.get("/messages/:messageId", indexController.messageDetailsGet);
-
-indexRouter.post("/messages", indexController.createMessagePost);
-
-indexRouter.delete("/messages/:messageId", indexController.messageDelete);
-
 export default indexRouter;
