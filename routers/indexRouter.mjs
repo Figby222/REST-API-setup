dc7a18294f5e56a5d201240952c89f0e6d85d8c9
Figby222 <figby222@outlook.com>
2024-10-05 08:14:47 -0700

    Move /users routes to usersRouter

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -3,16 +3,6 @@ import * as indexController from "../controllers/indexController.mjs";
 
 const indexRouter = Router();
 
-indexRouter.get("/users", indexController.indexRouteGet);
-
-indexRouter.get("/users/:userId", indexController.userDetailsGet);
-
-indexRouter.post("/users", indexController.indexRoutePost);
-
-indexRouter.put("/users/:userId", indexController.indexRoutePut);
-
-indexRouter.delete("/users/:userId", indexController.indexRouteDelete);
-
 indexRouter.get("/messages", indexController.messagesListGet);
 
 indexRouter.get("/messages/:messageId", indexController.messageDetailsGet);
